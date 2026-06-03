dce5d1be0efe2cc4b9add50156bbc12b7df249c5
Alex Gray <alex@symbioticconsulting.com.au>
2026-06-03 17:19:27 +1000

    ADCS comms code compiles now

--- Software/DOCKER_OBC/src/adcs.cpp
+++ Software/DOCKER_OBC/src/adcs.cpp
@@ -2,9 +2,6 @@
 #include <Arduino.h>
  
 ADCS_Handler_t hadcs;
-ADCS_HardwareHandler_t hwHandle;
-ADCS_StateHandle_t stateHandle;
-ADCS_hardwareInstruction_t controlHandle;
 
 void ADCS_Init(void)
 {
@@ -35,73 +32,29 @@ void ADCS_getTelemetry(void)
     }
 }
 
-void ADCS_getStateControllerUART(Stream* port)
-{
-    UART_msg_t msg;
-    if (UART_receive(port, &msg))
-    {
-        Serial.println("ADCS Controller Message Received!");
-        ADCS_processStateControllerUART(port, msg.id, msg.payload, msg.length);
-    }
-}
+// void ADCS_getStateControllerUART(Stream* port)
+// {
+//     UART_msg_t msg;
+//     if (UART_receive(port, &msg, DEFAULT_UART_TIMEOUT_US))
+//     {
+//         Serial.println("ADCS Controller Message Received!");
+//         ADCS_processStateControllerUART(port, msg.id, msg.payload, msg.length);
+//     }
+// }
 
 
-void ADCS_processStateControllerUART(Stream* port, uint8_t id,uint8_t* packet, uint8_t packet_len)
-{
-    uint8_t expected_length = ADCS_getRxPayloadLength(id);
-    if (packet_len != expected_length)
-    {
-        Serial.print("Bad ADCS packet length. RX=");
-        Serial.print(packet_len);
-        Serial.print(" Expected=");
-        Serial.println(expected_length);
-        return;
-    }
-    if (id == ADCS_PACKET_REQUEST_ID)
-    {
-        // Respond with hardware info
-    }
-    if (id == ADCS_PACKET_CONTROL_ID)
-    {
-        // Respond by loading into memory
-    }
-}
-
-void ADCS_hardwareRequestResponse(Stream* port)
-{
-    UART_msg_t msg;
-    msg.sof    = UART_SOF;
-    msg.id     = ADCS_PACKET_HW_DATA_ID;
-    msg.length = sizeof(ADCS_hardwareData_t);
-    memcpy(msg.payload, &hwHandle.hw_data, sizeof(ADCS_hardwareData_t));
-    UART_transmit(port, &msg);
-    Serial.println("Sent hardware data");
-}
-
-void ADCS_sendHardwareRequest(Stream* port)
-{
-    ADCS_requestHardwareData_t hwRequest;
-    hwRequest.request = true;
-
-    UART_msg_t msg;
-    msg.sof    = UART_SOF;
-    msg.id     = ADCS_PACKET_REQUEST_ID;
-    msg.length = sizeof(ADCS_requestHardwareData_t);
-    memcpy(msg.payload, &hwRequest, sizeof(ADCS_requestHardwareData_t));
-    UART_transmit(port, &msg);
-    Serial.println("Sent request for hardware data");
-}
-
-void ADCS_sendControlInstruction(Stream* port)
-{
-    UART_msg_t msg;
-    msg.sof    = UART_SOF;
-    msg.id     = ADCS_PACKET_CONTROL_ID;
-    msg.length = sizeof(ADCS_hardwareInstruction_t);
-    memcpy(msg.payload, &controlHandle, sizeof(ADCS_hardwareInstruction_t));
-    UART_transmit(port, &msg);
-    Serial.println("Sent control instruction");
-}
+// void ADCS_processStateControllerUART(Stream* port, uint8_t id,uint8_t* packet, uint8_t packet_len)
+// {
+//     uint8_t expected_length = ADCS_getRxPayloadLength(id);
+//     if (packet_len != expected_length)
+//     {
+//         Serial.print("Bad ADCS packet length. RX=");
+//         Serial.print(packet_len);
+//         Serial.print(" Expected=");
+//         Serial.println(expected_length);
+//         return;
+//     }
+// }
 
 void ADCS_processPacket(uint8_t id, uint8_t *payload, uint8_t payload_length)
 {
@@ -124,25 +77,13 @@ void ADCS_processPacket(uint8_t id, uint8_t *payload, uint8_t payload_length)
         memcpy(&hadcs.telemetry, payload, sizeof(ADCS_TelemetryPacket_t));
         Serial.println("ADCS telemetry updated");
     }
-    if (id == ADCS_PACKET_REQUEST_ID)
-    {
-        ADCS_hardwareRequestResponse(HW_TO_STATE_SERIAL);
-    }
-    if (id == ADCS_PACKET_HW_DATA_ID)
-    {
-        memcpy(&hwHandle.hw_data, payload, sizeof(ADCS_hardwareData_t));
-    }
-    if (id == ADCS_PACKET_CONTROL_ID)
-    {
-        memcpy(&hwHandle.ctrl, payload, sizeof(ADCS_hardwareInstruction_t));
-    }
 }
 
 
 void ADCS_receivePacket(Stream *port)
 {
     UART_msg_t msg;
-    UART_receive(port, &msg);
+    UART_receive(port, &msg, DEFAULT_UART_TIMEOUT_US);
     uint8_t len = ADCS_getRxPayloadLength(msg.id);
     ADCS_processPacket(msg.id, msg.payload, len);
 }
@@ -157,12 +98,6 @@ uint8_t ADCS_getRxPayloadLength(uint8_t id)
             return ADCS_PACKET_ACK_BYTES;
         case ADCS_PACKET_ERROR:
             return ADCS_PACKET_ERROR_BYTES;
-        case ADCS_PACKET_CONTROL_ID:
-            return sizeof(ADCS_hardwareInstruction_t);
-        case ADCS_PACKET_REQUEST_ID:
-            return sizeof(ADCS_requestHardwareData_t);
-        case ADCS_PACKET_HW_DATA_ID:
-            return sizeof(ADCS_hardwareData_t);
 
         default:
             return 0;
